1fbce68685104dc160bf4cebe8f7f6d64b2b7ca6
Ken Capolongo <kcapol1@students.towson.edu>
2016-04-02 15:14:44 -0400

    Added getter and setter methods for the point class variables of the Rectangle class.  Test passed.

--- Rectangle/src/edu/towson/cis/cosc603/project4/rectangle/Rectangle.java
+++ Rectangle/src/edu/towson/cis/cosc603/project4/rectangle/Rectangle.java
@@ -10,6 +10,7 @@ public class Rectangle {
 	private Point p1, p2;
 	private Double xLength, yLength;
 	
+	
 	/**
 	 * Instantiates a new rectangle.
 	 *
@@ -17,12 +18,47 @@ public class Rectangle {
 	 * @param p2 the p2
 	 */
 	Rectangle(Point p1, Point p2) {
-		this.p1 = p1;
-		this.p2 = p2;
-		xLength = Math.abs(p2.x - p1.x);
-		yLength = Math.abs(p2.y - p1.y);
+		this.setP1(p1);
+		this.setP2(p2);
 	}
 	
+	private void caclulateLengths() {
+		if((p1!=null) && (p2!=null)) {
+			xLength = Math.abs(p2.x - p1.x);
+			yLength = Math.abs(p2.y - p1.y);	
+		}
+	}
+		
+	/**
+	 * @return the p1
+	 */
+	public Point getP1() {
+		return p1;
+	}
+
+	/**
+	 * @param p1 the p1 to set
+	 */
+	public void setP1(Point p) {
+		this.p1 = p;
+		this.caclulateLengths();
+	}
+
+	/**
+	 * @return the p2
+	 */
+	public Point getP2() {
+		return p2;
+	}
+
+	/**
+	 * @param p2 the p2 to set
+	 */
+	public void setP2(Point p) {
+		this.p2 = p;
+		this.caclulateLengths();
+	}
+
 	/**
 	 * Gets the area.
 	 *
