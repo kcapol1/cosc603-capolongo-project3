5dc30384a84f38b5d1169ffab69678716ba39c90
Ken Capolongo <kcapol1@students.towson.edu>
2016-04-01 23:07:16 -0400

    Corrected half of the expected assertion values.  Resulted in four (4) failures.

--- Fibonacci/src/edu/towson/cis/cosc603/project4/fibonacci/FibonacciTest.java
+++ Fibonacci/src/edu/towson/cis/cosc603/project4/fibonacci/FibonacciTest.java
@@ -39,7 +39,7 @@ public class FibonacciTest {
 	 * Runs various equality tests against the Fibonacci class
 	 */
 	public void testFibonacciA() {	
-		assertEquals("0", 1, fibonacci.fibonacci(0));
+		assertEquals("0", 0, fibonacci.fibonacci(0));
 //		assertEquals("1", 1, fibonacci.fibonacci(1));
 //		assertEquals("2", 1, fibonacci.fibonacci(2));
 //		assertEquals("3", 1, fibonacci.fibonacci(3));
@@ -54,7 +54,7 @@ public class FibonacciTest {
 	 * Runs various equality tests against the Fibonacci class
 	 */
 	public void testFibonacciB() {	
-		assertEquals("1", 2, fibonacci.fibonacci(1));
+		assertEquals("1", 0, fibonacci.fibonacci(1));
 	}
 	
 	@Test 
@@ -62,7 +62,7 @@ public class FibonacciTest {
 	 * Runs various equality tests against the Fibonacci class
 	 */
 	public void testFibonacciC() {	
-		assertEquals("2", 3, fibonacci.fibonacci(2));
+		assertEquals("2", 1, fibonacci.fibonacci(2));
 	}
 	
 	@Test 
@@ -70,7 +70,7 @@ public class FibonacciTest {
 	 * Runs various equality tests against the Fibonacci class
 	 */
 	public void testFibonacciD() {	
-		assertEquals("3", 4, fibonacci.fibonacci(3));
+		assertEquals("3", 0, fibonacci.fibonacci(3));
 	}
 	
 	@Test 
@@ -78,7 +78,7 @@ public class FibonacciTest {
 	 * Runs various equality tests against the Fibonacci class
 	 */
 	public void testFibonacciE() {	
-		assertEquals("4", 5, fibonacci.fibonacci(4));
+		assertEquals("4", 3, fibonacci.fibonacci(4));
 	}
 	
 	@Test 
@@ -86,7 +86,7 @@ public class FibonacciTest {
 	 * Runs various equality tests against the Fibonacci class
 	 */
 	public void testFibonacciF() {	
-		assertEquals("5", 6, fibonacci.fibonacci(5));
+		assertEquals("5", 0, fibonacci.fibonacci(5));
 	}
 	
 	@Test 
@@ -94,7 +94,7 @@ public class FibonacciTest {
 	 * Runs various equality tests against the Fibonacci class
 	 */
 	public void testFibonacciG() {	
-		assertEquals("6", 7, fibonacci.fibonacci(6));
+		assertEquals("6", 8, fibonacci.fibonacci(6));
 	}
 	
 	@Test 
@@ -102,6 +102,6 @@ public class FibonacciTest {
 	 * Runs various equality tests against the Fibonacci class
 	 */
 	public void testFibonacciH() {	
-		assertEquals("7", 8, fibonacci.fibonacci(7));
+		assertEquals("7", 0, fibonacci.fibonacci(7));
 	}
 }
