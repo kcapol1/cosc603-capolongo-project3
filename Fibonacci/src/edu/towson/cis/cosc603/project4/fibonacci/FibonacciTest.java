e18a4f63028ba8c62769c597c78b863bcf776ab0
Ken Capolongo <kcapol1@students.towson.edu>
2016-04-01 23:01:58 -0400

    Added tearDown method and individual test methods for each test case. Changed expected values to cause failures.  Resulted in eight (8) failures.

--- Fibonacci/src/edu/towson/cis/cosc603/project4/fibonacci/FibonacciTest.java
+++ Fibonacci/src/edu/towson/cis/cosc603/project4/fibonacci/FibonacciTest.java
@@ -25,19 +25,83 @@ public class FibonacciTest {
 		fibonacci = new Fibonacci();
 	}
 	
+	@After 
+	/**
+	 * The tearDown method that destroys the necessary test objects.
+	 */
+	public void tearDown(){
+		fibonacci = null;
+	}
+	
 	
 	@Test 
 	/**
 	 * Runs various equality tests against the Fibonacci class
 	 */
-	public void testFibonacci() {	
-		assertEquals("0", 0, fibonacci.fibonacci(0));
-		assertEquals("1", 1, fibonacci.fibonacci(1));
-		assertEquals("2", 1, fibonacci.fibonacci(2));
-		assertEquals("3", 2, fibonacci.fibonacci(3));
-		assertEquals("4", 3, fibonacci.fibonacci(4));
-		assertEquals("5", 5, fibonacci.fibonacci(5));
-		assertEquals("6", 8, fibonacci.fibonacci(6));
-		assertEquals("7", 13, fibonacci.fibonacci(7));
+	public void testFibonacciA() {	
+		assertEquals("0", 1, fibonacci.fibonacci(0));
+//		assertEquals("1", 1, fibonacci.fibonacci(1));
+//		assertEquals("2", 1, fibonacci.fibonacci(2));
+//		assertEquals("3", 1, fibonacci.fibonacci(3));
+//		assertEquals("4", 3, fibonacci.fibonacci(4));
+//		assertEquals("5", 7, fibonacci.fibonacci(5));
+//		assertEquals("6", 8, fibonacci.fibonacci(6));
+//		assertEquals("7", 13, fibonacci.fibonacci(7));
+	}
+	
+	@Test 
+	/**
+	 * Runs various equality tests against the Fibonacci class
+	 */
+	public void testFibonacciB() {	
+		assertEquals("1", 2, fibonacci.fibonacci(1));
+	}
+	
+	@Test 
+	/**
+	 * Runs various equality tests against the Fibonacci class
+	 */
+	public void testFibonacciC() {	
+		assertEquals("2", 3, fibonacci.fibonacci(2));
+	}
+	
+	@Test 
+	/**
+	 * Runs various equality tests against the Fibonacci class
+	 */
+	public void testFibonacciD() {	
+		assertEquals("3", 4, fibonacci.fibonacci(3));
+	}
+	
+	@Test 
+	/**
+	 * Runs various equality tests against the Fibonacci class
+	 */
+	public void testFibonacciE() {	
+		assertEquals("4", 5, fibonacci.fibonacci(4));
+	}
+	
+	@Test 
+	/**
+	 * Runs various equality tests against the Fibonacci class
+	 */
+	public void testFibonacciF() {	
+		assertEquals("5", 6, fibonacci.fibonacci(5));
+	}
+	
+	@Test 
+	/**
+	 * Runs various equality tests against the Fibonacci class
+	 */
+	public void testFibonacciG() {	
+		assertEquals("6", 7, fibonacci.fibonacci(6));
+	}
+	
+	@Test 
+	/**
+	 * Runs various equality tests against the Fibonacci class
+	 */
+	public void testFibonacciH() {	
+		assertEquals("7", 8, fibonacci.fibonacci(7));
 	}
 }
