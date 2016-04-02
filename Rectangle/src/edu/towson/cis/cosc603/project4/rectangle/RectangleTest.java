968860f9a640175b2b9558ced1860f5192ee6a2f
Ken Capolongo <kcapol1@students.towson.edu>
2016-04-02 13:46:30 -0400

    Implemented class-level fixtures in the RectangleTest class.  Test passed.

--- Rectangle/src/edu/towson/cis/cosc603/project4/rectangle/RectangleTest.java
+++ Rectangle/src/edu/towson/cis/cosc603/project4/rectangle/RectangleTest.java
@@ -3,7 +3,9 @@ package edu.towson.cis.cosc603.project4.rectangle;
 import static org.junit.Assert.*;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -12,15 +14,16 @@ import org.junit.Test;
 public class RectangleTest {
 	
 	/** Declaring necessary test objects for {@link Rectangle} */
-	Rectangle rect1, rect2;
+	static Rectangle rect1, rect2;
 
 	/**
 	 * Initializes the necessary test objects for the test cases to use.
 	 *
 	 * @throws Exception the exception
 	 */
-	@Before
-	public void setUp() throws Exception {
+	@BeforeClass
+	public static void setUp() throws Exception {
+        System.out.println("setUp");
 		rect1 = new Rectangle(new Point(2.0, 2.0), new Point(4.0, 7.0));
 		rect2 = new Rectangle(new Point(2.0, 6.0), new Point(4.0, 3.0));
 	}
@@ -30,6 +33,7 @@ public class RectangleTest {
 	 */
 	@Test
 	public void testGetArea() {
+        System.out.println("testGetArea");
 		assertEquals(10.0, rect1.getArea(),0.001);
 		assertEquals(6.0, rect2.getArea(),0.001);
 	}
@@ -39,6 +43,7 @@ public class RectangleTest {
 	 */
 	@Test
 	public void testGetDiagonal() {
+        System.out.println("testGetDiagonal");
 		assertEquals(5.3852, rect1.getDiagonal(), 0.0001);
 		assertEquals(3.6056, rect2.getDiagonal(), 0.0001);
 	}
@@ -46,8 +51,9 @@ public class RectangleTest {
 	/**
 	 * Cleans up test objects after a test case is executed.
 	 */
-	@After
-	public void tearDown(){
+	@AfterClass
+	public static void tearDown(){
+        System.out.println("tearDown");
 		rect1 = null;
 		rect2 = null;
 	}
